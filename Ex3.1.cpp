1a112fb44f877d3a9fb6c95da2db086035b884c5
YinpuLi <yinpuli@icloud.com>
2019-07-02 09:20:58 -0400

    Update Ex3.1.cpp
    Note that the parameters are local to the function, in the previous version we are calculating the "ft" and "inch" in the void function, but output them in the main function, so we get "junk" numbers in the main.

--- Ex3.1.cpp
+++ Ex3.1.cpp
@@ -7,16 +7,15 @@ const int ft2inch = 12;
 void convert(int height, int ft, int inch){
    ft = height / ft2inch;
    inch = height % ft2inch;
+   cout << "Your height is " << ft << " feet and " << inch << " inches." << endl;
 }
 
 int main(){
   int height, ft, inch;
-  
+
   cout << "Enter your height(int, in inches): ";
-  int height;
   cin >> height;
 
   convert(height, ft, inch);
-  cout << "Your height is " << ft << " feet and " << inch << " inches." << endl;
   return 0;
 }
