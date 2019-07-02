69460c1db2e55eebcc29d2201ab28388b17726eb
YinpuLi <yinpuli@icloud.com>
2019-07-02 13:16:54 -0400

    Update Ex3.5.cpp
    changed the long long type of population into double

--- Ex3.5.cpp
+++ Ex3.5.cpp
@@ -2,24 +2,25 @@
 
 using namespace std;
 
-float fraction(long long w, long long u){
+float fraction(double w, double u){
     float p;
     p = u/w;
-    return p*100;
+    p *= 100;
+    return p;
 }
 
 int main(){
-    long long w, u;
+    double w, u;
     cout << "Enter the world's population: ";
     cin >> w;
     cout << "\nEnter the population of U.S. : ";
     cin >> u;
-    
+
     float p;
     p = fraction(w, u);
-    
+
     cout << "\nThe population of the US is " << p << "% of the world population." << endl;
-    
+
 
     return 0;
 }
